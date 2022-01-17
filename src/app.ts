063953e8fceae8714657b860dcbe3b0addd3e0bb
Rishabh1106 <rishabhnagar11061999@gmail.com>
2022-01-17 09:12:37 +0530

    shifted to buffer than image paths

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const keyJSON = {
     "type": process.env.type,
     "project_id": process.env.project_id,
     "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key.toString(),
+    "private_key": process.env.private_key,
     "client_email": process.env.client_email,
     "client_id": process.env.client_id,
     "auth_uri": process.env.auth_uri,
