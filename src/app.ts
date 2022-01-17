5a86c69ae6512ef06464ef5f0750ea55034cc5f0
Rishabh1106 <rishabhnagar11061999@gmail.com>
2022-01-17 09:02:11 +0530

    private key changed

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const keyJSON = {
     "type": process.env.type,
     "project_id": process.env.project_id,
     "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
+    "private_key": process.env.private_key.toString(),
     "client_email": process.env.client_email,
     "client_id": process.env.client_id,
     "auth_uri": process.env.auth_uri,
